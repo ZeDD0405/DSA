0e1880f627e4a6ce0818cae3510322a2da028ded
Sagar Kallimani <sagarkallimani0405@gmail.com>
2026-01-16 11:30:56 +0530

    Printing Frequence Of each element in an array/list with lindex and value method

--- freq.py
+++ freq.py
@@ -1,14 +1,27 @@
 #Printing Frequence Of each element in an array/list
 
 n = [1,2,4,5,7,8,5,4,7,3,6,3,7,0,5,8,8]
-x=len(n)
-freq=dict()
-for i in n:
-    if i in freq:
-        freq[i]+=1
-    else:
-        freq[i]=1
-        
-for key in freq:
-    print(freq[key])
+freq = {}
 
+for i in n:              # i is VALUE
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+
+for key in freq:
+    print(key, freq[key])
+
+
+n = [1,2,4,5,7,8,5,4,7,3,6,3,7,0,5,8,8]
+freq = {}
+x = len(n)
+
+for i in range(x):       # i is INDEX
+    if n[i] in freq:
+        freq[n[i]] += 1
+    else:
+        freq[n[i]] = 1
+
+for key in freq:
+    print(key, freq[key])
